6a5a19a166075686c7e80f119a5a346c676e2f7e
Panchal-Ashish <93699700+Panchal-Ashish@users.noreply.github.com>
2022-03-14 21:29:46 +0530

    Update smtplib intro.py

--- Day 32 smtplib, datetime, automated birthday email sender/smtplib intro.py
+++ Day 32 smtplib, datetime, automated birthday email sender/smtplib intro.py
@@ -1,8 +1,8 @@
 import smtplib
 # SMTP = simple mail transfer protocol
 
-my_email = "job.ashish28@gmail.com"
-password = "Ashish_28"
+my_email = "Your mail id"
+password = "Your Pswd"
 # before @, identity of user
 # after @, identity of service provider
 
@@ -11,7 +11,7 @@ password = "Ashish_28"
 connection = smtplib.SMTP("smtp.gmail.com", port= 587)    # every email provider has a different smtp server... google it
 connection.starttls()   # TLS = transport layer security... way of securing email to our email server
 connection.login(user= my_email, password= password)
-connection.sendmail(from_addr= my_email, to_addrs= "msd.pratham@gmail.com",
+connection.sendmail(from_addr= my_email, to_addrs= "receipient email id",
                     msg="Subject:Hello\n\n This is the content of my email")
 connection.close()      # can avoid this by using with open.... as...    similar to opening files
 
@@ -24,5 +24,5 @@ with smtplib.SMTP("smtp.gmail.com", port= 587) as connection:     # every email 
     connection.starttls()   # TLS = transport layer security... way of securing email to our email server
     connection.login(user= my_email, password= password)
     connection.sendmail(from_addr= my_email,
-                        to_addrs= "msd.pratham@gmail.com",
+                        to_addrs= "recipient email id",
                         msg="Subject:Hello\n\n This is the content of my email")
