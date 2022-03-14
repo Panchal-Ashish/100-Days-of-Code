59422c313923c29f2586bdd319c7de368595ad40
Panchal-Ashish <93699700+Panchal-Ashish@users.noreply.github.com>
2022-03-14 21:49:58 +0530

    Add files via upload

---
+++ Day 37 HTTP requests, Daily Habit Tracker/http requests types.py
@@ -0,0 +1,7 @@
+## --------- http request types: get, put, post, delete--------------##
+## requests.get() -- when we ask a external system for a particular piece of data and the system provides the data in
+##                   response.... eg. using APIs
+## requests.post() -- when we give the external system a piece of data and are not bothered much abt response except...
+##                   whether it was succssful or not ... Eg. saving data in google spreadsheets, post a post on insta
+## requests.put() -- when we update an existing piece of data in external system... eg. updating google sheets
+## requests.delete() -- when we want to delete a data in external systaem... eg. deleting a post on insta
