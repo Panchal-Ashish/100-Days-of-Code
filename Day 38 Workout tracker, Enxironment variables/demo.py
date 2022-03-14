9e2a03a72c5904e2a442cca5126a927ab4ba8780
Panchal-Ashish <93699700+Panchal-Ashish@users.noreply.github.com>
2022-03-14 21:52:51 +0530

    Add files via upload

---
+++ Day 38 Workout tracker, Enxironment variables/demo.py
@@ -0,0 +1,23 @@
+import os
+
+## To set an environment variable... Method 1
+# os.environ["API_KEY"] = '0456'
+# print(os.getenv("API_KEY"))
+
+
+## To use an environment variable... Method 2... using pycharms edit configurations
+## define the evnironment variavle in the environment variables section
+# a = os.environ['API']
+# print(a)
+
+
+## To use an environment variable... Method 3...
+## step 1: install 'env file' plugin... create and env file and store the keys
+## step 2: go to edit configuration > envfile
+## step 3: check the enable envfile option
+## step 4: click on + button to Add an env file...  add the env that we created
+
+# a = os.getenv("ENV_APP_KEY")
+# # a = os.environ.get('348c17c3fd7488d4eaf6c4c8f89dbb3a')
+# print(a)
+
---
+++ Day 38 Workout tracker, Enxironment variables/main.py
@@ -0,0 +1,64 @@
+import requests
+import datetime as dt
+import os
+
+AGE = 21
+GENDER = "male"
+WEIGHT_KG = 58
+HEIGHT_CM = 168
+
+TODAY = dt.datetime.now()
+DATE = TODAY.strftime("%d/%m/%Y")
+TIME = TODAY.strftime("%X")
+print(DATE)
+print(TIME)
+
+APP_ID = os.environ["ENV_APP_ID"]     # nutritionix   # #"57201ce9"
+APP_KEY = os.environ["ENV_APP_KEY"]        # nutritionionix  #"348c17c3fd7488d4eaf6c4c8f89dbb3a"
+
+exercise_endpoint =  "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = "https://api.sheety.co/45b16c9a9784d95e5305b68f0f4e7ec5/day38WorkoutTracker/workouts"
+
+HEADER = {
+    "x-app-id": APP_ID,
+    "x-app-key": APP_KEY
+}
+
+
+exercise_parameters = {
+    "query": input("Tell me which exercises you did: "),
+    "age": AGE,
+    "gender": GENDER,
+    "height_cm": HEIGHT_CM,
+    "weight_kg": WEIGHT_KG
+}
+
+response = requests.post(url= exercise_endpoint, json= exercise_parameters, headers= HEADER)
+data = response.json()
+print(data)
+print(type(data['exercises'][0]['nf_calories']))
+
+bearer_headers = {
+"Authorization": "Bearer Ashish"
+}
+for exercise in data['exercises']:
+    sheety_parameters = {
+        "workout": {
+            "date": DATE,
+            "time": TIME,
+            "exercise": exercise['name'].title(),
+            "duration": exercise['duration_min'],
+            "calories": exercise['nf_calories']
+        }
+    }
+    ## BASIC AUTHENTICATIONS-------------------------
+    sheety_response = requests.post(url=sheety_endpoint, json=sheety_parameters, auth= ("Ashish","Ashish"))
+    print(sheety_response.text)
+
+    ## BEARER AUTHENTICATIONS------------------------------------------- first change auth settings in site
+    # sheety_response = requests.post(url= sheety_endpoint, json=sheety_parameters, headers= bearer_headers)
+    # print(sheety_response.text)
+
+
+
+
